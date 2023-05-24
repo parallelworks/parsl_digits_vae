dc30c4be97457e823e89717d980f6320503e93c2
avidalto <avidalto@hawk.iit.edu>
2023-05-24 15:23:02 -0500

    Add design explorer

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import json
+import json, os
 import parsl
 print(parsl.__version__, flush = True)
 
@@ -6,7 +6,41 @@ import parsl_utils
 from parsl_utils.config import config, read_args
 from parsl_utils.data_provider import PWFile
 
+import pandas as pd
+
 from workflow_apps import train, generate_data
+
+def prepare_design_explorer():
+    """
+    Prepare the CSV and HTML files for Design Explorer
+    """
+    cwd = os.getcwd()
+    csv_file_path = f"{cwd}/generated_data/design_explorer.csv"
+
+    print('\n\nPreparing Design Explorer files')
+    print('Creating CSV file', flush = True)
+    df = pd.read_csv(csv_file_path)
+    df['img:digit'] = os.getcwd() + '/' + df['img:digit'] 
+    df.to_csv(csv_file_path, index = False)
+    
+    print('Creating HTML file', flush = True)
+    html_file = f'''
+<html style="overflow-y:hidden;background:white">
+    <a 
+        style="font-family:sans-serif;z-index:1000;position:absolute;top:15px;right:0px;margin-right:20px;font-style:italic;font-size:10px" 
+        href="/DesignExplorer/index.html?datafile={csv_file_path}&colorby=1" 
+        target="_blank">Open in New Window
+    </a>
+    <iframe 
+        width="100%" 
+        height="100%" 
+        src="/DesignExplorer/index.html?datafile={csv_file_path}&colorby=1" 
+        frameborder="0">
+    </iframe>
+</html>
+'''
+    with open("design_explorer.csv", "w") as f:
+        f.write(html_file)
 
 
 if __name__ == '__main__':
@@ -71,3 +105,5 @@ if __name__ == '__main__':
 
     generate_data_fut.result()
 
+    # Design Explorer:
+    prepare_design_explorer()
